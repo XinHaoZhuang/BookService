a1b08f5a6326df775c834d3720496620a3d8b10c
庄 <zhx517064524@outlook.com>
2018-10-11 08:41:30 +0800

    20181011

---
+++ WebApplication3/Migrations/Configuration.cs
@@ -0,0 +1,55 @@
+namespace WebApplication3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using WebApplication3.Models;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication3.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "WebApplication3.Models.MovieDBContext";
+        }
+
+        protected override void Seed(WebApplication3.Models.MovieDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            context.Movies.AddOrUpdate(i => i.Title,
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+                 new Movie
+                 {
+                     Title = "Ghostbusters 2",
+                     ReleaseDate = DateTime.Parse("1986-2-23"),
+                     Genre = "Comedy",
+                     Price = 9.99M
+                 },
+                   new Movie
+                   {
+                       Title = "Rio Bravo",
+                       ReleaseDate = DateTime.Parse("1959-4-15"),
+                       Genre = "Western",
+                       Price = 3.99M
+                   }
+                );
+        }
+    }
+}
